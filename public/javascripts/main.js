f3200916bb5ee89d85b99c5590de67888705508b
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-03-25 23:50:09 +0000

    initial commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,327 @@
+var board
+var gameID
+var name
+var turn
+var player
+
+
+$(document).ready(function() {
+  doModal()
+})
+
+function doModal() {
+  $('#myModal').modal('show')
+}
+
+function closeModal(){
+  setInterval(getBoard, 3000)
+  runPage()
+}
+
+function modalSubmit() {
+  name = $('#name').val()
+  gameID = $('#gameID').val()
+
+  if (name == '' || gameID == '') {
+    $('#alert').html('<div class="alert alert-warning">Name and game ID cannot be left blank.</div>');
+    return
+  }
+
+  if ($('#newGame').is(':checked')) {
+    var data = {
+      "session" : gameID,
+      "player0" : name,
+      "turn": 0,
+      "board": [[],[],[],[],[],[],[]]
+    }
+    $.ajax({
+      url: '/board',
+      method: 'POST',
+      contentType: "application/json;odata=verbose",
+      data: JSON.stringify(data),
+      headers: {
+        "Accept": "application/json;odata=verbose"
+      },
+      success: function(res) {
+        console.log("Game Created");
+        console.log(res);
+        $('#myModal').modal('toggle')
+        player = 0
+        closeModal();
+      },
+      failure: function() {
+        console.log('Server error. Try again')
+      }
+    })
+  }
+  else {
+    $.ajax({
+      url: '/board?session=' + gameID,
+      method: 'GET',
+      headers: { "Accept": "application/json; odata=verbose" },
+      success: function(json) {
+        console.log("Finding board:");
+        console.log(json);
+        if(json==null || json==undefined){
+          console.log("Board not found");
+          $('#alert').html('<div class="alert alert-warning">Game ID does not exist. Please create a new game or try again.</div>');
+        }
+        else {
+          var data = {
+            "session" : gameID,
+            "player1" : name
+          }
+          $.ajax({
+            url: '/board',
+            method: 'POST',
+            contentType: "application/json;odata=verbose",
+            data: JSON.stringify(data),
+            headers: {
+              "Accept": "application/json;odata=verbose"
+            },
+            success: function(res) {
+              console.log("Game joined");
+              console.log(res)
+              $('#myModal').modal('hide')
+              player = 1
+              closeModal();
+            },
+            failure: function() {
+              console.log('Server error. Try again');
+            }
+          })
+        }
+      },
+      failure: function(){
+        console.log('Server error. Try again');
+      }
+    })
+  }
+}
+
+function getBoard() {
+  $.ajax({
+    url: '/board?session=' + gameID,
+    method: 'GET',
+    headers: { "Accept": "application/json; odata=verbose" },
+    success: function(json) {
+      console.log("Board Recieved");
+      paintBoard(json)
+    },
+    failure: function(){
+      console.log('Server error. Try again');
+    }
+  })
+}
+
+function paintBoard(json) {
+  console.log("Making Board");
+  board = json.board
+  turn = json.turn
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      if (board[i][j] == 0) {
+        $('#' + (j+1).toString() + (i+1).toString() + ' .circle').css('background', 'red')
+      }
+      if (board[i][j] == 1) {
+        $('#' + (j+1).toString() + (i+1).toString() + ' .circle').css('background', 'black')
+      }
+    }
+  }
+  //$('#turn').text("Player "+((turn+1)==1? "One":"Two")+"'s Turn");
+  $('#turn').text(((turn+1)==1? json.player0 : json.player1)+"\'s Turn");
+  $('#prompt').text(((turn==player)? "It's your turn. Go ahead and move!":"It's not your turn. You're going to have to wait for the other player."));
+  testFinish();
+}
+
+function runPage() {
+  turn = 0
+  $('.col1').click(function() {
+    if (turn == player && board[0].length < 6)
+    {
+      dropPiece(0)
+      madeMove(0)
+    }
+    if (turn == player && board[0].length == 6)
+    {
+      $('#prompt').text('Column is full. Try a different column.')
+    }
+  })
+  $('.col2').click(function() {
+    if (turn == player && board[1].length < 6)
+    {
+      dropPiece(1)
+      madeMove(1)
+    }
+    if (turn == player && board[1].length == 6)
+    {
+      $('#prompt').text('Column is full. Try a different column.')
+    }
+  })
+  $('.col3').click(function() {
+    if (turn == player && board[2].length < 6)
+    {
+      dropPiece(2)
+      madeMove(2)
+    }
+    if (turn == player && board[2].length == 6)
+    {
+      $('#prompt').text('Column is full. Try a different column.')
+    }
+  })
+  $('.col4').click(function() {
+    if (turn == player && board[3].length < 6)
+    {
+      dropPiece(3)
+      madeMove(3)
+    }
+    if (turn == player && board[3].length == 6)
+    {
+      $('#prompt').text('Column is full. Try a different column.')
+    }
+  })
+  $('.col5').click(function() {
+    if (turn == player && board[4].length < 6)
+    {
+      dropPiece(4)
+      madeMove(4)
+    }
+    if (turn == player && board[4].length == 6)
+    {
+      $('#prompt').text('Column is full. Try a different column.')
+    }
+  })
+  $('.col6').click(function() {
+    if (turn == player && board[5].length < 6)
+    {
+      dropPiece(5)
+      madeMove(5)
+    }
+    if (turn == player && board[5].length == 6)
+    {
+      $('#prompt').text('Column is full. Try a different column.')
+    }
+  })
+  $('.col7').click(function() {
+    if (turn == player && board[6].length < 6)
+    {
+      dropPiece(6)
+      madeMove(6)
+    }
+    if (turn == player && board[6].length == 6)
+    {
+      $('#prompt').text('Column is full. Try a different column.')
+    }
+  })
+}
+
+function dropPiece(col) {}
+
+function madeMove(col) {
+  var data = {
+    "session" : gameID,
+    "move" : {
+      "player" : player,
+      "column" : col
+    }
+  }
+  $.ajax({
+    url: '/board',
+    method: 'POST',
+    contentType: "application/json;odata=verbose",
+    data: JSON.stringify(data),
+    headers: {
+      "Accept": "application/json;odata=verbose"
+    },
+    success: function() {
+      getBoard()
+    },
+    failure: function(){
+      console.log('server error. Try again');
+    }
+  })
+}
+
+function testFinish(){
+  test_horizontal=function(x,y,player){
+    if(x<4){
+      var found_win=true;
+      for(var i=x; i<x+4; i++){
+        if(board[i][y] != player){
+          found_win=false;
+          break;
+        }
+      }
+    return found_win;
+    } else return false;
+  }
+  test_vertical=function(x,y,player){
+    if(y<3){
+      var found_win=true;
+      for(var i=y; i<y+4; i++){
+        if(board[x][i] != player){
+          found_win=false;
+          break;
+        }
+      }
+    return found_win;
+    } else return false;
+  }
+  test_diagonal_1=function(x,y,player){
+    if(x<4 && y<3){
+      var found_win=true;
+      for(var i=0; i<4; i++){
+        if(board[x+i][y+i] != player){
+          found_win=false;
+          break;
+        }
+      }
+    return found_win;
+    } else return false;
+  }
+  test_diagonal_2=function(x,y,player){
+    if(x>2 && y<3){
+      var found_win=true;
+      for(var i=0; i<4; i++){
+        if(board[x-i][y+i] != player){
+          found_win=false;
+          break;
+        }
+      }
+    return found_win;
+    } else return false;
+  }
+  for(var play=0; play<=1; play++){
+    for (var i = 0; i < board.length; i++) {
+      for (var j = 0; j < board[i].length; j++) {
+        console.log("testing ("+i.toString()+","+j.toString()+")");
+        if(test_horizontal(i,j,play) || test_vertical(i,j,play) || test_diagonal_1(i,j,play) || test_diagonal_2(i,j,play)){
+          if(play==player) win();
+          else lose();
+          j=board[i].length;
+          i=board.length;
+          play=1;
+          break;
+        }
+      }
+    }
+  }
+}
+function endGame(){
+  var url="board?session="+gameID;
+  $.ajax({
+      url: url,
+      type: "DELETE",
+      success: function(data,textStatus){
+         $("#done").html(textStatus);
+      }
+  })
+}
+function win(){
+  //endGame();
+  window.location = "/win";
+}
+function lose(){
+  endGame();
+  window.location="/lose";
+}
---
+++ routes/index.js
@@ -0,0 +1,109 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/ConnectFourDB');
+var connectFourSchema = mongoose.Schema({ 
+  session: {type: String, required: true},
+  board: [],
+  turn: {type: Number, min: 0, max: 2, default: 2}, 
+  player0: {type: String, default: "Unknown"},
+  player1: {type: String, default: "Unknown"},
+  updated: {type: Date, default: Date.now}
+});
+var Board = mongoose.model('Board', connectFourSchema);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected');
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Connect Four' });
+});
+
+router.get('/tester', function(req, res, next) {
+  res.render('tester',{ title: 'Testing Platform'});
+});
+
+router.get('/win', function(req, res, next) {
+  res.render('finish', { title: 'You Win!!' });
+});
+
+router.get('/lose', function(req, res, next) {
+  res.render('finish', { title: 'You Lose.' });
+});
+
+add_to_column=function(board, column, player){
+  if(board[column].length<6)
+    board[column].push(player);
+  return board;
+}
+
+check_format=function(body){
+  var output ={};
+  if(body.board != undefined && body.board != null && typeof(body.board)==='object' && body.board.length==7){
+    var good_board=true;
+    for(var i=0; i<7; i++){
+      if(typeof(body.board[i]) != "object" || body.board[i].length==undefined || body.board[i].length == null || body.board[i].length>=7 || body.board[i].length<0){ good_board=false; break;}
+      for(var j=0; j<body.board[i].length; j++){
+        if(typeof(body.board[i][j]) != "number" || !(body.board[i][j] == 1 || body.board[i][j] == 0)){good_board=false; break;}
+      }
+    }
+    if(good_board==true) output.board=body.board;
+  }
+  if(body.turn != undefined && body.turn != null && typeof(body.turn)==='number' && (body.turn === 0 || body.turn===1)) output.turn=body.turn;
+  if(body.session != undefined && body.session != null && typeof(body.session)==='string' && body.session.length <25) output.session=body.session;
+  if(body.player0 != undefined && body.player0 != null && typeof(body.player0)==='string' && body.player0.length <25) output.player0=body.player0;
+  if(body.player1 != undefined && body.player1 != null && typeof(body.player1)==='string' && body.player1.length <25) output.player1=body.player1;
+  return output;
+}
+
+router.post('/board', function(req, res, next) {
+  if(req.body == undefined || req.body == null || typeof(req.body) != 'object' || req.body.session == undefined || req.body.session == null) res.sendStatus(206);
+  Board.findOne({'session': req.body.session}, function(error, foundsession) {
+    if(foundsession && !error){
+      var conditions = { session: req.body.session };
+      if(req.body.move != undefined && req.body.move != null && typeof(req.body.move) ==='object' && req.body.move.player != undefined && req.body.move.player != null && typeof(req.body.move.player) === 'number' && ((req.body.move.player === 0 && foundsession.turn===0) || (req.body.move.player === 1 && foundsession.turn===1)) && req.body.move.column != undefined && req.body.move.column != null && typeof(req.body.move.column) === 'number' && req.body.move.column>=0 && req.body.move.column<=7){
+        var update = { $set: { turn: ((req.body.move.player==0)? 1 : 0), board: add_to_column(foundsession.board, req.body.move.column, req.body.move.player)}};
+      } else {
+        var update = { $set: check_format(req.body)};
+      }
+      var options = { multi: true };
+      Board.update(conditions, update, options, function(err,num){
+        if(err) return console.error(err); 
+      });
+      return;
+    } else if(!foundsession) {
+      var newboard = new Board(check_format(req.body)); 
+      newboard.save(function(err, post) {
+      if (err) return console.error(err);
+      return;
+      });
+    } else {
+      console.error(error);
+      return;
+    }
+  });
+  Board.findOne({'session': req.body.session}, function(err,foundboard) {
+    if (err) return console.error(err);
+    else {
+      res.json(foundboard);
+    }
+  })
+});
+
+router.get('/board',function(req, res, next) {
+  Board.findOne({'session': req.query.session}, function(err,foundboard) {
+    if (err) return console.error(err);
+    else {
+      res.json(foundboard);
+    }
+  })
+});
+
+router.delete('/board', function(req, res, next) {
+  Board.findOne({'session':req.query.session}).remove().exec();
+});
+
+module.exports = router;
